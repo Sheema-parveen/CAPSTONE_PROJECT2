ffcf5a86f29d37e48c05647c9881a1271f2fa508
Sheema-parveen <sheemaparveenfeb14@gmail.com>
2023-08-28 19:54:59 +0530

    All values used in this project are stored here

---
+++ Test_Information/info.py
@@ -0,0 +1,43 @@
+class Info:
+    url="https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    url2="https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
+    Username="Admin"
+    password = "admin123"
+
+    #job
+    job_titleinput = "Quality Assurance"
+    payname='o+'
+    empstatus='parttime'
+    category='team lead'
+    shift='day'
+
+    #organization
+    place='salem'
+    structname='QA'
+
+    #qualification
+    skill='RUBY ON RAILS'
+    education='PG'
+    licence='certified QA CQA'
+    language='germany'
+    membership='ANTPP'
+    
+    #nationalities
+    nation='India'
+
+    #branding
+    pg1='#b24581'
+    pg2='#40bdae'
+    pc='#c97272'
+    sc='#c6bee5'
+    font1='#ed9696'
+    font2='#eacdcd'
+
+    #configuration
+    mail="sheema@gmail.com"
+    mail2='sheem123845s@gmail.com'
+    name='sheema'
+    nameredirect='OrangeHRM Mobile App'
+    redirect='com.orangehrm.opensource://oauthredirect'
+   
+
